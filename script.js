8aac66ff0b74a991055508384abff29de3f022de
Jake A Barnwell <jb16@mit.edu>
2014-11-11 21:29:51 -0500

    Regex searching for keywords to calculate damages. Right now just finding key phrases.

--- script.js
+++ script.js
@@ -182,6 +182,8 @@ var damageNotInScalings = function(labels) {
 var showTooltipsNotHavingDamageKeywordsBeforeDamage = function(data) {
 	results = {};
 
+	var numZero = 0;
+
 	var results = "<ol>";
 	for(var champ in data) {
 		// console.log(champ);
@@ -198,7 +200,7 @@ var showTooltipsNotHavingDamageKeywordsBeforeDamage = function(data) {
 
 			var tooltip = spell.sanitizedTooltip;
 
-			var keywords = ["take","takes","taking","deal","deals","dealt","dealing","does","doing","suffer","suffers","suffering"];
+			var keywords = ["shoots","slicing","take","takes","taking","deal","deals","dealt","dealing","does","doing","suffer"];
 			var keywords_withSquiggle = [];
 			for(var k in keywords) {
 				keywords_withSquiggle[k] = keywords[k] + " {{";
@@ -215,14 +217,25 @@ var showTooltipsNotHavingDamageKeywordsBeforeDamage = function(data) {
 			}
 
 			//if the spell mentions damage in its sanitized description...
-			if(hasDamageKeyword(keywords,tooltip) && notFalsePositive(data[champ]["name"], s)) {
+			if(trueNegative(data[champ]["name"], s) || (hasDamageKeyword(keywords,tooltip) && notFalsePositive(data[champ]["name"], s))) {
 
 				//if the spell does not have 'damamge' in its level scalings labels...
 				// if(damageNotInScalings(spell.leveltip.label)) {
-				if(!hasDamageKeyword(keywords_withSquiggle,tooltip)) {
+				// if(!hasDamageKeyword(keywords_withSquiggle,tooltip)) {
+				if(false) {
 					results += "<li><b><h3>" + champ + " " + spellLetter + ": " + tooltip + "</h3></b></li>";
-				} else {
+				} else { //The majority of cases fall here; these are the ones I want
 					results += "<li>" + champ + " " + spellLetter + ": " + tooltip + "</li>";
+					var thing = getPortion(keywords,tooltip);
+					results += "<li> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" + champ + " " + spellLetter + ": " + thing + "</li>";
+
+
+
+					if(thing.length === 0) {
+						numZero += 1;
+						console.log("Champ: " + data[champ]["name"] + ", spell: " + s);
+						results += "<li><h1> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" + champ + " " + spellLetter + ": " + thing + "</h1></li>";
+					}
 
 				}
 			}
@@ -234,7 +247,7 @@ var showTooltipsNotHavingDamageKeywordsBeforeDamage = function(data) {
 
 
 	}
-
+	console.log("numZero is " + numZero);
 	$("#content").html(results);
 }
 
@@ -245,6 +258,23 @@ var hasDamageKeyword = function(keywords, tooltip) {
 		}
 	}
 	return false;
+}
+
+var getPortion = function(keywords, tooltip) {
+	var tooltip = tooltip.toLowerCase();
+
+	var results = [];
+
+	for(var k in keywords) {
+		var key = keywords[k];
+		var regex = new RegExp("("+key+" )([A-Za-z0-9:.;()\\[\\]\\+]+ ){0,4}([{(]+ )([eaf0-9().%{} \\+]*)([)}%]+ )(['%A-Za-z0-9:.;()\\[\\]\\+]+ ){0,12}(damage)", "g"); 
+		var matches = tooltip.match(regex);
+		if(matches && matches.length) {
+			results = results.concat(matches);
+		}
+	}
+
+	return results;
 }
 
 var notFalsePositive = function(champName, spellNumber) {
@@ -289,6 +319,47 @@ var notFalsePositive = function(champName, spellNumber) {
 		return false;
 	}
 	return true;
+}
+
+var trueNegative = function(champName, spellNumber) {
+	var spellName = "";
+	switch(spellNumber) {
+		case 0:
+			spellName = "Q";
+			break;
+		case 1:
+			spellName = "W";
+			break;
+		case 2:
+			spellName = "E";
+			break;
+		case 3:
+			spellName = "R";
+			break;
+		case 4:
+			spellName = "Q2";
+			break;
+		case 5:
+			spellName = "W2";
+			break;
+		case 6:
+			spellName = "E2";
+			break;
+		case 7:
+			spellName = "R2";
+			break;
+		default:
+			break;
+	}
+
+	var spell = champName + spellName;
+
+	var trueNegatives =
+		["AzirE","MalzaharW","NunuR","AkaliE","LucianQ","EzrealW"];
+	if(trueNegatives.indexOf(spell) >= 0) {
+		return true;
+	}
+	return false;
 }
 
 //Only looks at scaling coefficients
